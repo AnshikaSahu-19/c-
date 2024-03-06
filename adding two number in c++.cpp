354065865f622d2986befa063b7b927b4fd63042
AnshikaSahu-19 <162312560+AnshikaSahu-19@users.noreply.github.com>
2024-03-06 15:22:38 +0530

    Add files via upload

---
+++ adding two number in c++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+
+{                             
+
+    int a;
+int b;
+int sum;
+    
+    cout<<"Enter the value of a  ";
+    cin>>a;
+    cout<<"Enter the value of b ";
+    cin>>b;
+  int   c=a+b;
+  cout<<"sum of "<<a<<" and "<<b<<" ="  <<c;
+    
+return 0;
+}
